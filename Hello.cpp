c80f159d79fd8a6f0f69c9530ed701ce8d5155c9
evhook <anonymous1986222@gmail.com>
2026-05-09 17:45:53 +0530

    Refactor Hello World program to use 'using namespace std' for cleaner syntax

--- Hello.cpp
+++ Hello.cpp
@@ -1,4 +1,15 @@
+//Without using namespace std;
+
+// #include <iostream>
+// int main(){
+//     std::cout<<"Hello World\n"<<std::endl;
+// }
+
+//With using namespace std;
+
 #include <iostream>
+using namespace std;
 int main(){
-    std::cout<<"Hello World\n"<<std::endl;
+    cout<<"Hello World\n"<<endl;
+    return 0;
 }
