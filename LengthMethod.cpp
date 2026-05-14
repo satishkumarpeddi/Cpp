40d615d04f44774fadfd7cf65abf3dac13475a49
evhook <anonymous1986222@gmail.com>
2026-05-14 15:43:50 +0530

    Add LengthMethod.cpp to demonstrate string length calculation

---
+++ LengthMethod.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <algorithm>
+#include <iostream>
+#include <string>
+int main(){
+    std::string word = "The anonymous 198622";
+    std::cout << "The length of the String : " << word.length() << std::endl;
+    return 0;
+}
