02152d23ad550606ca68244f9511f1c0c5154606
evhook <anonymous1986222@gmail.com>
2026-05-14 15:52:35 +0530

    Add EmptyMethod.cpp to demonstrate string emptiness check

---
+++ EmptyMethod.cpp
@@ -0,0 +1,9 @@
+#include <string>
+#include <algorithm>
+#include <iostream>
+using namespace std;
+int main(){
+    string word = "Anonymous198622";
+    cout << "Is String Empty?: " << word.empty() << endl;
+    return 0;
+}
