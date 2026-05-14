5c6d6cd8ddf1e384f7cd79f94e3fb10a519e6eab
evhook <anonymous1986222@gmail.com>
2026-05-14 15:48:45 +0530

    Add SizeMethod.cpp to demonstrate string size calculation

---
+++ SizeMethod.cpp
@@ -0,0 +1,9 @@
+#include <string>
+#include <iostream>
+#include <stdio.h>
+#include <algorithm>
+int main(){
+    std::string word = "word size.";
+    std::cout << "The word length : " << word.size() << std::endl;
+    return 0;
+}
